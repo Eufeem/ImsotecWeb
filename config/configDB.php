48520cb57036e988c6d3308802958fd03801c687
Carlos García <eufeemgr@gmail.com>
2018-05-17 17:32:26 -0500

    Frontend
    Se agrego la parte gráfica del formulario html

---
+++ config/configDB.php
@@ -0,0 +1,24 @@
+<?php
+    /**
+     *
+     */
+    class DataBase {
+        private $host = "localhost";
+        private $dbName = "imsotec";
+        private $userName = "root";
+        private $password = "";
+        public  $conn;
+
+        public function getConnection() {
+            $this->conn = null;
+            try {
+                $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbName, $this->userName, $this->password);
+                $this->conn->exec("set names utf8");
+            } catch (PDOException $exception) {
+                echo "Connection error: " . $exception->getMessage();
+            }
+            return $this->conn;
+        }
+    }
+
+ ?>
